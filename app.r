c4b52c9d785824f5b09e1880a71f9eb378d10a7e
Julio Spairani <jspairani@gmail.com>
2022-01-25 23:59:43 -0300

    tutorial 2: update 2: sections filled
    header, nav, footer
    added svg logo
    added css title adn logo clases
    documentation: changed section definitions

--- app.r
+++ app.r
@@ -75,10 +75,6 @@ makePage <- function (title, subtitle, contents) {
   contents)
 }
 
-header <- "header"
-navigation <- "navigation"
-footer <- "footer"
-
 #' layout: for transformin into a dashboard.
 #' warning: there are 3 globals invocations that are characters: header, navigation, footer
 #' they define the sections elements.
@@ -98,7 +94,66 @@ layout <- function(mainUI){
   )
 }
 
-# filter definition -------------------------------------------------------
+
+# section definition ------------------------------------------------------
+
+
+# ui: header definition ---------------------------------------------------
+
+header <- tagList(
+  img(src = "logo.svg", class = "logo"),
+  div(Text(variant = "xLarge", "Sales Reps Analysis"), class = "title"),
+  CommandBar(
+    items = list(
+      CommandBarItem("New", "Add", subitems = list(
+        CommandBarItem("Email message", "Mail", key = "emailMessage", href = "mailto:me@example.com"),
+        CommandBarItem("Calendar event", "Calendar", key = "calendarEvent")
+      )),
+      CommandBarItem("Upload sales plan", "Upload"),
+      CommandBarItem("Share analysis", "Share"),
+      CommandBarItem("Download report", "Download")
+    ),
+    farItems = list(
+      CommandBarItem("Grid view", "Tiles", iconOnly = TRUE),
+      CommandBarItem("Info", "Info", iconOnly = TRUE)
+    ),
+    style = list(width = "100%")))
+
+# ui: navigation definition ---------------------------------------------------
+
+navigation <- Nav(
+  groups = list(
+    list(links = list(
+      list(name = 'Home', url = '#!/', key = 'home', icon = 'Home'),
+      list(name = 'Analysis', url = '#!/other', key = 'analysis', icon = 'AnalyticsReport'),
+      list(name = 'shiny.fluent', url = 'http://github.com/Appsilon/shiny.fluent', key = 'repo', icon = 'GitGraph',target="_blank"),
+      list(name = 'shiny.react', url = 'http://github.com/Appsilon/shiny.react', key = 'shinyreact', icon = 'GitGraph',target="_blank"),
+      list(name = 'Appsilon', url = 'http://appsilon.com', key = 'appsilon', icon = 'WebAppBuilderFragment',target="_blank")
+    ))
+  ),
+  initialSelectedKey = 'home',
+  styles = list(
+    root = list(
+      height = '100%',
+      boxSizing = 'border-box',
+      overflowY = 'auto'
+    )
+  )
+)
+
+# ui: footer definition ---------------------------------------------------
+
+footer <- Stack(
+  horizontal = TRUE,
+  horizontalAlign = 'space-between',
+  tokens = list(childrenGap = 20),
+  Text(variant = "medium", "Built with ❤ by Appsilon", block=TRUE),
+  Text(variant = "medium", nowrap = FALSE, "If you'd like to learn more, reach out to us at hello@appsilon.com"),
+  Text(variant = "medium", nowrap = FALSE, "All rights reserved.")
+)
+
+
+## filter definition -------------------------------------------------------
 # adding  more filter components
 filters <- Stack(
   tokens = list(childrenGap = 10),
@@ -130,9 +185,9 @@ filters <- Stack(
 )
 
 
-# analysis page definition ------------------------------------------------
+## analysis page definition ------------------------------------------------
 
-# refactored ui to page definition
+## refactored ui to page definition
 analysis_page <- makePage(
   "Sales representatives",
   "Best performing reps",
